7239cd0a7148f871d42f3c656755d7a7994f17d8
gurneet33 <gurneet.me@gmail.com>
2018-11-14 19:53:54 -0500

    js changes made

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#sumbit-button").on("click",function(event){
+    event.preventDefault();
+  
+    var formName = $("#name-input").val().trim();
+    var formRole = $("#role-input").val().trim();
+    var formStartDate = $("#start-date").val().trim();
+    var formRate = $("#monthly-rate").val().trim();
+    
+})
